4a716a7bd99e39c30046eae552c390ad6ce4a3ed
poovarasanmu <poovarasanmu151@gmail.com>
2023-08-17 20:39:25 +0530

    committed by ps

---
+++ Collection/CollectionDemo.java
@@ -0,0 +1,83 @@
+package input.output;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class CollectionDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		ArrayList al = new ArrayList();
+//		al.add("hi");
+//		al.add(5);
+//		al.add(true);
+//		al.add(10.4f);
+//		al.add('c');
+//		System.out.println(al);
+//		
+//		al.add(2,"kalai");
+//		System.out.println(al);
+//		
+//		
+//		
+//		
+//		ArrayList al2 = new ArrayList();
+//		al2.add(100);
+//		al2.addAll(al);
+//		System.out.println(al2);
+//		
+//		System.out.println(al2.size());
+//		System.out.println(al2.containsAll(al));
+//		System.out.println(al2.get(3));
+//		System.out.println(al2.indexOf("kalai"));
+//		
+//		
+//		List ll = al2.subList(2, 6);
+//		System.out.println(ll);
+//		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		LinkedList al = new LinkedList();
+		al.add("hi");
+		al.add(5);
+		al.add(true);
+		al.add(10.4f);
+		al.add('c');
+		System.out.println(al);
+		
+		al.add(2,"kalai");
+		System.out.println(al);
+		
+		
+		
+		
+		LinkedList al2 = new LinkedList();
+		al2.add(100);
+		al2.addAll(al);
+		System.out.println(al2);
+		
+		System.out.println(al2.size());
+		System.out.println(al2.containsAll(al));
+		System.out.println(al2.get(3));
+		System.out.println(al2.indexOf("kalai"));
+		
+		
+		List ll = al2.subList(2, 6);
+		System.out.println(ll);
+		
+		
+		
+	}
+
+}
